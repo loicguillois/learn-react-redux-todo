59feec6cb15e35d7512ed74b423770642ead463f
Loic Guillois <contact@loicguillois.fr>
2016-09-21 15:56:18 +0200

    editing..

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -4,18 +4,47 @@ function setState(state, newState) {
   return state.merge(newState);
 }
 
-function toggleComplete(state, itemId) {
-  // We find the index associated with the itemId
-  const itemIndex = state.get('todos').findIndex(
+function findItemIndex(state, itemId) {
+  return state.get('todos').findIndex(
     (item) => item.get('id') === itemId
   );
+}
 
-  // We update the todo at this index
+// We can refactor the toggleComplete function to use findItemIndex
+function toggleComplete(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
   const updatedItem = state.get('todos')
     .get(itemIndex)
     .update('status', status => status === 'active' ? 'completed' : 'active');
 
-  // We update the state to account for the modified todo
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function editItem(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', true);
+
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function cancelEditing(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', false);
+
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function doneEditing(state, itemId, newText) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', false)
+    .set('text', newText);
+
   return state.update('todos', todos => todos.set(itemIndex, updatedItem));
 }
 
@@ -31,6 +60,12 @@ export default function(state = Map(), action) {
       return toggleComplete(state, action.itemId);
     case 'CHANGE_FILTER':
       return changeFilter(state, action.filter);
+    case 'EDIT_ITEM':
+      return editItem(state, action.itemId);
+    case 'CANCEL_EDITING':
+      return cancelEditing(state, action.itemId);
+    case 'DONE_EDITING':
+      return doneEditing(state, action.itemId, action.newText);
   }
   return state;
 }
