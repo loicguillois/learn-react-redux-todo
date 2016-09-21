3482ff085045c2eaf42931c5bd6f6ecabb5696c6
Loic Guillois <contact@loicguillois.fr>
2016-09-21 11:53:14 +0200

    adding callbacks for clicks/double clicks

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,15 +12,22 @@ export default class TodoItem extends React.Component {
     });
     return <li className={itemClass}>
       <div className="view">
+        // We add an onClick handler on the checkbox
         <input type="checkbox"
                className="toggle"
-               defaultChecked={this.props.isCompleted}/>
-        <label htmlFor="todo">
+               defaultChecked={this.props.isCompleted}
+               onClick={() => this.props.toggleComplete(this.props.id)}/>
+        // We add a ref attribute to the label to facilitate the testing
+        // The onDoubleClick handler is unsurprisingly called on double clicks
+        <label htmlFor="todo"
+               ref="text"
+               onDoubleClick={() => this.props.editItem(this.props.id)}>
           {this.props.text}
         </label>
-        <button className="destroy"></button>
+        <button className="destroy"
+                onClick={() => this.props.deleteItem(this.props.id)}></button>
       </div>
-        <TextInput />
-      </li>
+      <TextInput />
+    </li>
   }
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -21,7 +21,11 @@ export default class TodoList extends React.Component {
           <TodoItem key={item.get('text')}
                     text={item.get('text')}
                     isCompleted={this.isCompleted(item)}
-                    isEditing={item.get('editing')} />
+                    isEditing={item.get('editing')}
+            // We pass down the callback functions
+                    toggleComplete={this.props.toggleComplete}
+                    deleteItem={this.props.deleteItem}
+                    editItem={this.props.editItem}/>
         )}
       </ul>
     </section>
